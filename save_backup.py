a1336851c1efaf28bf38b696b7a5ac31184cfb92
Pseudowitchy <natalieqh@live.com>
2022-08-09 13:30:02 -0400

    Update save_backup.py
    Added comment for updating to your personal save location and changing date format of folder.

--- save_backup.py
+++ save_backup.py
@@ -4,8 +4,12 @@ import shutil
 
 date = datetime.now()
 
-source = Path(r"C:/Program Files (x86)/Steam/userdata/53828396/1446780/remote")
-# source = Path(r"C:/Program Files (x86)/Steam/userdata/32342647/1446780/remote")
+# This will place the backed up save files into new folder named for the current date in MM-DD-YYYY format.
+# If you wish to change the date format you can change the order at the end of the line below, for 2 digit date use %y instead of %Y.
 target = Path() / date.strftime("%m-%d-%Y")
+
+# Edit this line to where your saves are located, the 1446780 folder is the folder for the game, the folder that contains it is dependant on your steam profile.
+source = Path(r"C:/Program Files (x86)/Steam/userdata/32342647/1446780/remote")
+
 
 shutil.copytree(source, target)
