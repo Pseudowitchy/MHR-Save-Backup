9149a9f07fdd2e33525363aeb95b7a04e8f9ce01
Pseudowitchy <natalieqh@live.com>
2022-08-09 14:42:52 -0400

    Should detect save locations
    The code should now autodetect the location of save files if Steam is installed in C:/Program Files (x86). This should allow it to be used by most users.

--- save_backup.py
+++ save_backup.py
@@ -3,13 +3,14 @@ from datetime import datetime
 import shutil
 
 date = datetime.now()
+steam = Path(r"C:/Program Files (x86)/Steam/userdata")
 
 # This will place the backed up save files into new folder named for the current date in MM-DD-YYYY format.
 # If you wish to change the date format you can change the order at the end of the line below, for 2 digit date use %y instead of %Y.
 target = Path() / date.strftime("%m-%d-%Y")
 
-# Edit this line to where your saves are located, the 1446780 folder is the folder for the game, the folder that contains it is dependant on your steam profile.
-source = Path(r"C:/Program Files (x86)/Steam/userdata/32342647/1446780/remote")
-
+saves = [p for p in steam.rglob("1446780/remote")]
+for save in saves:
+    source = save
 
 shutil.copytree(source, target)
