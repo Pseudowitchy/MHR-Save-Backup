44db0bc7474062bab51e09bda3bf93b7eb0eaaad
Pseudowitchy <natalieqh@live.com>
2022-08-11 15:06:11 -0400

    Added Backup GUI and directory picker
    + GUI
    + Directory Picker
    + Check if there is already an existing backup for todays date; ask if to files should be overwritten.

--- save_backup.py
+++ save_backup.py
@@ -1,16 +1,80 @@
+import os
+import shutil
+import tkinter as tk
 from pathlib import Path
 from datetime import datetime
-import shutil
+from tkinter import Label, filedialog, Button, Entry, messagebox
 
-date = datetime.now()
 steam = Path(r"C:/Program Files (x86)/Steam/userdata")
 
-# This will place the backed up save files into new folder named for the current date in MM-DD-YYYY format.
-# If you wish to change the date format you can change the order at the end of the line below, for 2 digit date use %y instead of %Y.
-target = Path() / date.strftime("%m-%d-%Y")
 
-saves = [p for p in steam.rglob("1446780/remote")]
-for save in saves:
-    source = save
+class Gui:
+    def __init__(self, master):
+        self.master = master
+        self.create_window()
 
-shutil.copytree(source, target)
+    def create_window(self, location=steam):  # Bits inside the window
+
+        global browse_bar_text
+        browse_bar_text = tk.StringVar(value=location)
+
+        browse_info = Label(text="""Find your Steam/userdata folder, this is Steam's install
+              directory, not for Monster Hunter Rise""")
+        browse_info.grid(row=0, columnspan=5)
+
+        browse_bar = Entry(font=('arial', 9), text=browse_bar_text,
+                           width=52, border=3)
+        browse_bar.grid(row=1, column=0, columnspan=4, padx=2)
+
+        browse_button = Button(text="Browse Files", width=10, height=1,
+                               command=self.Select_Directory)
+        browse_button.grid(row=1, column=4, padx=1)
+
+        backup_button = Button(text="Backup Saves", width=10,
+                               command=lambda: self.Backup_Run(location))
+        backup_button.grid(row=2, columnspan=5, pady=5)
+
+    def Select_Directory(self):
+        # Lets the user select the directory that the code will search
+        global browse_bar_text
+        steam = Path(filedialog.askdirectory(initialdir=browse_bar_text))
+        browse_bar_text = steam
+        self.create_window(steam)
+
+    def Backup_Run(self, location):
+        # This will place the backed up save files into new folder named for
+        # the current date in MM-DD-YYYY format.
+        # If you wish to change the date format you can change the order at the
+        # end of the line below, for 2 digit date use %y instead of %Y.
+        date = datetime.now()
+        target = Path() / date.strftime("%m-%d-%Y")
+
+        if "userdata" in location.__str__():
+            saves = [p for p in location.rglob("1446780/remote")]
+            for save in saves:
+                source = save
+            if os.path.exists(target):
+                duplicate_backup = messagebox.askquestion(
+                    "Backup Already Exists",
+                    """A backup has been created for todays date.
+                    Would you like to overwrite the old backup
+                    with the new one?""")
+                if duplicate_backup == 'yes':
+                    shutil.rmtree(target)
+                    shutil.copytree(source, target)
+                else:
+                    pass
+            else:
+                shutil.copytree(source, target)
+        else:
+            messagebox.showerror(
+                "Erorr", "Ensure you selected the correct folder.")
+
+
+if __name__ == "__main__":
+    window = tk.Tk()
+    window.geometry("460x300")
+    window.title("MH Rise Save Backup")
+    window.wm_resizable(width=False, height=False)
+    my_gui = Gui(window)
+    window.mainloop()
